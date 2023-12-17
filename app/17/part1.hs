d94949bae79f79848c76694d08400a25b651d3dc
Milan Boers <mail@milanboers.nl>
2023-12-17 08:50:49 +0100

    Day 17

---
+++ app/17/part1.hs
@@ -0,0 +1,52 @@
+import Algorithm.Search (dijkstra)
+import Data.Char (digitToInt)
+import Data.Matrix (Matrix, ncols, nrows, (!))
+import qualified Data.Matrix as Matrix
+import Data.Maybe (fromJust, isJust)
+
+type Field = Matrix Int
+
+type Pos = (Int, Int)
+
+data Dir = U | D | L | R deriving (Show, Eq, Enum, Ord)
+
+type State = (Pos, Dir, Int) -- pos, dir, # in dir
+
+move :: Dir -> Pos -> Pos
+move U (r, c) = (r - 1, c)
+move D (r, c) = (r + 1, c)
+move L (r, c) = (r, c - 1)
+move R (r, c) = (r, c + 1)
+
+oppositeDir :: Dir -> Dir
+oppositeDir U = D
+oppositeDir D = U
+oppositeDir L = R
+oppositeDir R = L
+
+neighbors :: Field -> State -> [State]
+neighbors field (pos, dir, nDir) =
+  [ ((r, c), newDir, newNDir)
+    | newDir <- [U ..],
+      oppositeDir newDir /= dir,
+      let (r, c) = move newDir pos,
+      isJust $ Matrix.safeGet r c field,
+      let newNDir = if dir == newDir then nDir + 1 else 0,
+      newNDir < 3
+  ]
+
+cost :: Field -> State -> State -> Int
+cost field _ (pos, _, _) = field ! pos
+
+end :: Field -> State -> Bool
+end field ((r, c), _, _) = r == nrows field && c == ncols field
+
+findAnswer :: Field -> Int
+findAnswer field = fst . fromJust . dijkstra (neighbors field) (cost field) (end field) $ ((1, 1), R, 0)
+
+main :: IO ()
+main = do
+  contents <- getContents
+  let input = lines contents
+  let field = Matrix.fromLists $ (map . map) digitToInt input
+  print $ findAnswer field
